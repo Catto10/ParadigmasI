de3d2dee2b8f60fe01831abcbeb541ae4b901247
Luis Enrique Diaz Catarino <luisenriquediazcatarino@gmail.com>
2022-09-09 09:36:12 -0500

    prueba

---
+++ src/Figuras/Figura.java
@@ -0,0 +1,21 @@
+package Figuras;
+
+public abstract class Figura {
+
+
+
+    public abstract double calcularArea();
+
+    public abstract double calcularPerimetro();
+
+
+
+    @Override
+
+    public String toString(){
+
+        return "Figura{}";
+
+    }
+
+}
